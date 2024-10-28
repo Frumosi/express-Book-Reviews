67e891df33421d12b1db358a14909d7f789f1989
Frumosi <kkone.devops@gmail.com>
2024-10-28 03:32:31 -0400

    Updated

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -13,6 +13,8 @@ const userMatches = users.filter((user) => user.username === username);
 
 const authenticatedUser = (username,password)=>{ //returns boolean
 //write code to check if username and password match the one we have in records.
+const matchingUsers = users.filter((user) => user.username === username && user.password === password);
+  return matchingUsers.length > 0;
 
 
 }
@@ -23,25 +25,16 @@ regd_users.post("/login", (req,res) => {
     const username = req.body.username;
     const password = req.body.password;
   
-    // Check if username or password is missing
-    if (!username || !password) {
-        return res.status(404).json({ message: "Error logging in" });
-    }
-    // Authenticate user
     if (authenticatedUser(username, password)) {
-        // Generate JWT access token
-        let accessToken = jwt.sign({
-            data: password
-        }, 'access', { expiresIn: 60 * 60 });
-        // Store access token and username in session
-        req.session.authorization = {
-            accessToken, username
-        }
-        return res.status(200).send("User successfully logged in");
-    } else {
-        return res.status(208).json({ message: "Invalid Login. Check username and password" });
+      let accessToken = jwt.sign({data:password}, "access", {expiresIn: 3600});
+      req.session.authorization = {accessToken,username};
+      return res.status(200).send("User successfully logged in");
+    }
+    else {
+      return res.status(208).json({message: "Invalid username or password"});
     }
   });
+  
 
 //  Task 8
 //  Add a book review
